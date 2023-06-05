11c71b03c6c23abc23cec60b6213b1d5c24ae236
SHON PURDY <SHONPURDY@GMAIL.COM>
2023-06-05 12:13:13 -0700

    Check for right answer function

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
-var randomNumberGenerator = function (size) {
-    return Math.ceil(Math.random() * size);
-  }
-  
-  var questionGenerator = function () {
-    var question = {};
-    var num1 = randomNumberGenerator(10);
-    var num2 = randomNumberGenerator(10);
+$(document).ready(function(){
+    var currentQuestion;
+    var randomNumberGenerator = function (size) {
+      return Math.ceil(Math.random() * size);
+    }
     
-    question.answer = num1 + num2;
-    question.equation = String(num1) + " + " + String(num2);
+    var questionGenerator = function () {
+      var question = {};
+      var num1 = randomNumberGenerator(10);
+      var num2 = randomNumberGenerator(10);
+      
+      question.answer = num1 + num2;
+      question.equation = String(num1) + " + " + String(num2);
+      
+      return question;
+    }
     
-    return question;
-  }
+    currentQuestion = questionGenerator();
+    $('#equation').text(currentQuestion.equation);
 
-  console.log(questionGenerator());
-console.log(questionGenerator());
+    var renderNewQuestion = function () {
+      currentQuestion = questionGenerator();
+      $('#equation').text(currentQuestion.equation);  
+    }
+    var checkAnswer = function (userInput, answer) {
+      if(userInput === answer) {
+        renderNewQuestion();
+        $('#user-input').val('');
+      }
+    }
+    $('#user-input').on('keyup', function () {
+      checkAnswer(Number($(this).val()), currentQuestion.answer);
+    });
+    renderNewQuestion();
+
+  });
