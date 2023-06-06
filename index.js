5523cbf990ff2f68366d54563141bf4b66674f77
SHON PURDY <SHONPURDY@GMAIL.COM>
2023-06-06 10:17:15 -0700

    Add comments and conditions for "-" and "/"

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+  // iniitialize variables
     var currentQuestion;
     var timeLeft = 10;
     $('#time-left').text(timeLeft);
@@ -6,7 +8,7 @@ $(document).ready(function(){
     var highscore = 0;
     var interval;
 
-
+    // get the max-number for operations from user input (slider)
     var slider = document.getElementById("slider");
     var sliderValue = document.getElementById("sliderValue");
 
@@ -19,6 +21,7 @@ $(document).ready(function(){
       return Math.ceil(Math.random() * size);
     }
     
+    // check to see what operation box is checked, then call the renderNewQuestion function
     const checkboxes = document.querySelectorAll('input[name="operation"]');
     checkboxes.forEach(checkbox => {
       checkbox.addEventListener('change', function() {
@@ -34,14 +37,20 @@ $(document).ready(function(){
       const checkboxes = document.querySelectorAll('input[name="operation"]');
       checkboxes.forEach(checkbox => {
         if (checkbox.checked) {
-          var num1 = randomNumberGenerator(slider.value);
+          var num1 = randomNumberGenerator(slider.value); // get random number based on slider
           var num2 = randomNumberGenerator(slider.value);
+
+          // update question based on which checkbox is checked.
           switch (checkbox.value) {
             case '+':
               question.answer = num1 + num2;
-              question.equation = `${num1} + ${num2}`;
+              question.equation = `${num1} + ${num2}`; 
               break;
             case '-':
+              if (num1 < num2) {
+                num1 = num2;
+                num2 = num1;
+              }
               question.answer = num1 - num2;
               question.equation = `${num1} - ${num2}`;
               break;
@@ -50,9 +59,15 @@ $(document).ready(function(){
               question.equation = `${num1} * ${num2}`;
               break;
             case '/':
+              // make sure you get two numbers that, when divided by each other, equal a whole number.
+              while (num1 % num2 !== 0) {
+                num1 = randomNumberGenerator(slider.value); 
+                num2 = randomNumberGenerator(slider.value);
+              }
               question.answer = num1 / num2;
               question.equation = `${num1} / ${num2}`;
               break;
+
             default:
               // Handle any other cases or errors here
               break;
@@ -66,20 +81,22 @@ $(document).ready(function(){
     
     currentQuestion = questionGenerator();
 
+    // creates a new question and places it into HTML
     var renderNewQuestion = function () {
       currentQuestion = questionGenerator();
       $('#equation').text(currentQuestion.equation);  
     }
+
     var checkAnswer = function (userInput, answer) {
       if(userInput === answer) {
-        $('#user-input').val('');
-        updateTimeLeft(1);
-        updateScore(1);
+        $('#user-input').val(''); // reset the user input field
+        updateTimeLeft(+1); // add one second for correct answer
+        updateScore(+1); // add +1 on score for correct answer
         renderNewQuestion();
       }
     }
     $('#user-input').on('keyup', function () {
-      checkAnswer(Number($(this).val()), currentQuestion.answer);
+      checkAnswer(Number($(this).val()), currentQuestion.answer); // compare user input value with correct answer
     });
     renderNewQuestion();
 
@@ -88,6 +105,7 @@ $(document).ready(function(){
             if (timeLeft === 0) {
                 resetGame();
             }
+            //count down each second from 10, then when it hits 0 clear the interval.
             interval = setInterval(function () {
                 updateTimeLeft(-1);
                 if (timeLeft === 0) {
@@ -98,16 +116,19 @@ $(document).ready(function(){
         }
     }
 
+      //update time left and add it to HTML.
       var updateTimeLeft = function (amount) {
         timeLeft += amount;
         $('#time-left').text(timeLeft);
       }
 
+      // check for user input and start the timer.
       $('#user-input').on('keyup', function () {
         startGame();
         checkAnswer(Number($(this).val()), currentQuestion.answer);
       });
 
+      // keep track of high score based on previous scores
       var updateScore = function (amount) {
         score += amount;
         $('#score').text(score);
